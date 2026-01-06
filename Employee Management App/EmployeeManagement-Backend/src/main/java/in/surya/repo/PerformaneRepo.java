6f2a47989d7bc9de802b0f2c8e938830d86ab5b4
thesuryakumar <152575282+thesuryakumar@users.noreply.github.com>
2026-01-06 22:14:11 +0530

    Delete Employee Management App directory

--- Employee Management App/EmployeeManagement-Backend/src/main/java/in/surya/repo/PerformaneRepo.java
+++
@@ -1,9 +0,0 @@
-package in.surya.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.surya.entity.PerformanceReview;
-
-public interface PerformaneRepo extends JpaRepository<PerformanceReview, Integer>{
-
-}
